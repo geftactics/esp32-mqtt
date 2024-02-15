d2b04cb46d46d3d3b72e26f5738c1e22822acbc1
Geoff Kendal <gkendal@williamhill.co.uk>
2024-02-15 09:16:23 +0000

    Adding WiFiManager

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,30 @@
-#include "config.h"
-#include "heltec.h"
-#include <WiFiClientSecure.h>
-#include <MQTTClient.h>
+#include <config.h>
+#include <heltec.h>
 #include <ArduinoJson.h>
-#include "WiFi.h"
+#include <base64.h>
+#include <FS.h>
+#include <MQTTClient.h>
+#include <SPIFFS.h>
+#include <WiFiClientSecure.h>
+#include <WiFiManager.h>
+
 
 #define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
 
+
 WiFiClientSecure wifi_client = WiFiClientSecure();
 MQTTClient mqtt_client = MQTTClient(256);
 
+
 void incomingMessageHandler(String &topic, String &payload) {
-  Serial.println("Message received!");
-  Serial.println("Topic: " + topic);
+  Serial.println("Message received on '" + topic + "'");
   Serial.println("Payload: " + payload);
   Heltec.display->clear();
   Heltec.display->drawString(0, 0, payload); 
   Heltec.display->display();
 }
+
 
 void publishMessage()
 {
@@ -27,29 +33,25 @@ void publishMessage()
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer);
   mqtt_client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
-  Serial.println("Sent a message");
+  Serial.println("Sent message");
 }
+
 
 void connect()
 {
-  WiFi.mode(WIFI_STA); 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  Serial.println("Connecting to Wi-Fi...");
-  while (WiFi.status() != WL_CONNECTED){
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
+  Heltec.display->clear();
+  Heltec.display->drawString(0, 0, "Connecting to MQTT..."); 
+  Heltec.display->display();
 
   wifi_client.setCACert(AWS_CERT_CA);
   wifi_client.setCertificate(AWS_CERT_CRT);
-  wifi_client.setPrivateKey(AWS_CERT_PRIVATE);
+  wifi_client.setPrivateKey(AWS_CERT_KEY);
 
   mqtt_client.begin(AWS_IOT_ENDPOINT, 8883, wifi_client);
   mqtt_client.onMessage(incomingMessageHandler);
 
-  Serial.print("Connecting to AWS IOT...");
+  Serial.print("Connecting to MQTT...");
   while (!mqtt_client.connect(THINGNAME)) {
     Serial.print(".");
     delay(100);
@@ -57,26 +59,47 @@ void connect()
   Serial.println();
 
   if(!mqtt_client.connected()){
-    Serial.println("AWS IoT Timeout!");
+    Serial.println("MQTT Timeout!");
     return;
   }
 
   mqtt_client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
-  Serial.println("AWS IoT Connected!");
-}
+  Serial.println("MQTT Connected!");
 
-void setup() {
-  Serial.begin(115200);
-  Heltec.begin(true, false, false);
   Heltec.display->clear();
   Heltec.display->drawString(0, 0, "Waiting for message..."); 
   Heltec.display->display();
+
 }
 
+
+void setup() {
+
+  Serial.begin(115200);
+
+  Heltec.begin(true, false, false);
+  Heltec.display->clear();
+  Heltec.display->drawString(0, 0, "Waiting for Wi-Fi setup..."); 
+  Heltec.display->display();
+
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("ESP-MQTT");
+
+  Serial.println("Connected to Wi-Fi!");
+  Heltec.display->clear();
+  Heltec.display->drawString(0, 0, "Connected to Wi-Fi!"); 
+  Heltec.display->display();
+
+}
+
+
 void loop() {
+
   if (!mqtt_client.connected()) {
     connect();
   }
+
   mqtt_client.loop();
-  delay(1000);
+  delay(100);
+
 }
